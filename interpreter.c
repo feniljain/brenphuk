d481df88619ced8f861bfe73025818a041b07042
feniljain <fkjainco@gmail.com>
2024-03-08 23:20:40 +0530

    refactor: remove using engine struct and rely on global vars
    - add more optimizations build flags

--- interpreter.c
+++ interpreter.c
@@ -53,13 +53,11 @@ int main(int argc, char *argv[]);
 
 // interpreter impl
 
-typedef struct {
-  int pointer;
-  char tape[TAPE_SIZE];
-  int brackets_loc[TAPE_SIZE][2];
-} engine;
+int pointer;
+char tape[TAPE_SIZE];
+int brackets_loc[TAPE_SIZE][2];
 
-void print_bracket_arr(engine *eng, int stop_len) {
+void print_bracket_arr(int stop_len) {
   int i = 0;
 
   while (true) {
@@ -68,25 +66,25 @@ void print_bracket_arr(engine *eng, int stop_len) {
         break;
       }
     } else {
-      if (eng->brackets_loc[i][0] == -1) {
+      if (brackets_loc[i][0] == -1) {
         break;
       }
     }
 
-    DBG_PRINTF("brackets_loc[%d]: [: %d ]: %d", i, eng->brackets_loc[i][0],
-               eng->brackets_loc[i][1]);
+    DBG_PRINTF("brackets_loc[%d]: [: %d ]: %d", i, brackets_loc[i][0],
+               brackets_loc[i][1]);
     i += 1;
   }
 }
 
 // could I use a hashmap here? Well for small programs and small
 // number of brackets, a linear search will be faster (tsoding ftw)
-void fill_brackets_loc(engine *eng, char *prog, int prog_len) {
+void fill_brackets_loc(char *prog, int prog_len) {
   int i = 0, idx = 0;
   while (i < prog_len) {
     switch (prog[i]) {
     case '[': {
-      eng->brackets_loc[idx][0] = i;
+      brackets_loc[idx][0] = i;
 
       int brackets_depth = 0;
       for (int j = i; j < prog_len; j++) {
@@ -97,7 +95,7 @@ void fill_brackets_loc(engine *eng, char *prog, int prog_len) {
         }
 
         if (brackets_depth == 0) {
-          eng->brackets_loc[idx][1] = j;
+          brackets_loc[idx][1] = j;
           break;
         }
       }
@@ -119,7 +117,7 @@ void fill_brackets_loc(engine *eng, char *prog, int prog_len) {
 
 enum Bracket { OPEN = 1, CLOSE = 2 };
 
-int get_matching_bracket_idx(engine *eng, int idx, int br) {
+int get_matching_bracket_idx(int idx, int br) {
   int check_idx = -1, ret_idx = -1;
   switch (br) {
   case OPEN:
@@ -135,43 +133,66 @@ int get_matching_bracket_idx(engine *eng, int idx, int br) {
     break;
   }
 
-  for (int i = 0; eng->brackets_loc[i][0] != -1; i++) {
-    if (idx == eng->brackets_loc[i][check_idx]) {
-      return eng->brackets_loc[i][ret_idx];
+  for (int i = 0; brackets_loc[i][0] != -1; i++) {
+    if (idx == brackets_loc[i][check_idx]) {
+      return brackets_loc[i][ret_idx];
     }
   }
 
   return -1;
 }
 
-int exec(engine *eng, char *prog, int prog_len) {
-  DBG_PRINT(prog);
-  int i = 0;
+double benchmark_results[8][2]; // 7 commands, 1st column for count, 2nd for
+                                // exec avg time
 
-  fill_brackets_loc(eng, prog, prog_len);
-  print_bracket_arr(eng, -1);
+void print_benchmark_results(void) {
+  for (int i = 0; i < 8; i++) {
+    DBG_PRINTF("%d command executed %f times with %f total exec time", i,
+               benchmark_results[i][0], benchmark_results[i][1]);
+  }
+}
+
+// TODO(feniljain): shift all operations to functions and try flamegraph
+int exec(char *prog, int prog_len) {
+  DBG_PRINT(prog);
+  int i = 0, b_idx; // , cnt = 0;
+
+  // clock_t start, end;
+  // double cpu_time_used;
+
+  fill_brackets_loc(prog, prog_len);
+  print_bracket_arr(-1);
 
   while (i < prog_len && i >= 0) {
+    // int dataptr = pointer;
+    // cnt++;
+    // start = clock();
     switch (prog[i]) {
     case '>':
-      eng->pointer++;
+      b_idx = 0;
+      pointer++;
       break; // TODO(feniljain): Add bound checks here
     case '<':
-      eng->pointer--;
+      b_idx = 1;
+      pointer--;
       break; // TODO(feniljain): Add bound checks here
     case '+':
-      eng->tape[eng->pointer]++;
+      b_idx = 2;
+      tape[pointer]++;
       break; // I don't care about int overflow here
     case '-':
-      eng->tape[eng->pointer]--;
+      b_idx = 3;
+      tape[pointer]--;
       break;
     case '.':
-      printf("%c", eng->tape[eng->pointer]);
+      b_idx = 4;
+      printf("%c", tape[pointer]);
       break;
     case ',': {
+      b_idx = 5;
       char ch;
       scanf("%c", &ch);
-      eng->tape[eng->pointer] = ch;
+      tape[pointer] = ch;
       break;
     }
     case '[':
@@ -179,14 +200,16 @@ int exec(engine *eng, char *prog, int prog_len) {
       int br = OPEN;
       bool condition;
       if (prog[i] == '[') {
-        condition = eng->tape[eng->pointer] == 0;
+        b_idx = 6;
+        condition = tape[pointer] == 0;
       } else {
-        condition = eng->tape[eng->pointer] != 0;
+        b_idx = 7;
+        condition = tape[pointer] != 0;
         br = CLOSE;
       }
 
       if (condition) {
-        int matching_idx = get_matching_bracket_idx(eng, i, br);
+        int matching_idx = get_matching_bracket_idx(i, br);
         if (matching_idx == -1) {
           ABORT("could not find matching bracket, bug in bracket finding code");
         }
@@ -202,19 +225,30 @@ int exec(engine *eng, char *prog, int prog_len) {
     }
 
     i++;
+
+    (void)b_idx;
+
+    // end = clock();
+    // cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    // benchmark_results[b_idx][0]++;
+    // benchmark_results[b_idx][1] += cpu_time_used;
+    // if (cnt == 200000000) {
+    //   break;
+    // }
   }
 
+  // print_benchmark_results();
   return 0;
 }
 
-void reset(engine *eng) {
-  memset(eng->tape, 0, sizeof eng->tape);
-  eng->pointer = 0;
+void reset(void) {
+  memset(tape, 0, sizeof tape);
+  pointer = 0;
 
-  memset(eng->brackets_loc, -1, sizeof eng->brackets_loc);
+  memset(brackets_loc, -1, sizeof brackets_loc);
 }
 
-int repl(engine *eng) {
+int repl(void) {
   printf("Brainfuck Repl\n");
   for (;;) {
     char *prog = (char *)NULL;
@@ -225,25 +259,25 @@ int repl(engine *eng) {
     } else if (!strcmp(prog, "")) {
       goto nxt_itr;
     } else if (!strcmp(prog, "reset")) {
-      reset(eng);
+      reset();
     } else if (strstr(prog, "print") != NULL) {
       strsep(&prog, " "); // print cmd
 
       char *cmd = strsep(&prog, " ");
 
       if (!strcmp(cmd, "idx")) {
-        printf("pointer index: %d", eng->pointer);
+        printf("pointer index: %d", pointer);
       } else if (!strcmp(cmd, "curr")) {
-        printf("value at current index: %d", eng->tape[eng->pointer]);
+        printf("value at current index: %d", tape[pointer]);
       } else {
         int locs = atoi(cmd);
         for (int i = 0; i < locs; i++) {
-          printf("%d: %d | ", i, eng->tape[i]);
+          printf("%d: %d | ", i, tape[i]);
         }
       }
       printf("\n");
     } else {
-      int errno = exec(eng, prog, (int)strlen(prog));
+      int errno = exec(prog, (int)strlen(prog));
       if (errno != 0) {
         printf("error: %s\n", strerr(errno));
       }
@@ -258,61 +292,60 @@ int repl(engine *eng) {
   return 0;
 }
 
-int tests(engine *eng) {
-  exec(eng, "+++", 3);
-  assert(eng->tape[0] == 3);
+int tests(void) {
+  exec("+++", 3);
+  assert(tape[0] == 3);
 
-  reset(eng);
+  reset();
   DBG_PRINT("tests::test 1 done");
 
   // opening bracket for loop test
-  exec(eng, "[++]", 4);
-  assert(eng->tape[0] == 0);
+  exec("[++]", 4);
+  assert(tape[0] == 0);
 
-  reset(eng);
+  reset();
   DBG_PRINT("tests::test 2 done");
 
   // nested opening bracket for loop test
-  exec(eng, "[++[++]]", 8);
-  assert(eng->tape[0] == 0);
+  exec("[++[++]]", 8);
+  assert(tape[0] == 0);
 
-  reset(eng);
+  reset();
   DBG_PRINT("tests::test 3 done");
 
   // closing bracket for loop test
-  exec(eng, "+++[-]", 6);
-  assert(eng->tape[0] == 0);
+  exec("+++[-]", 6);
+  assert(tape[0] == 0);
 
-  reset(eng);
+  reset();
   DBG_PRINT("tests::test 4 done");
 
-  exec(eng, ">+++++++++[<++++++>-]<...>++++++++++.", 37);
-  assert(eng->tape[0] == 54);
-  assert(eng->tape[1] == 10);
+  exec(">+++++++++[<++++++>-]<...>++++++++++.", 37);
+  assert(tape[0] == 54);
+  assert(tape[1] == 10);
 
-  reset(eng);
+  reset();
   DBG_PRINT("tests::test 5 done");
 
   // part of hello world
-  exec(eng, "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]", 49);
-  assert(eng->tape[0] == 0);
-  assert(eng->tape[1] == 0);
-  assert(eng->tape[2] == 72);
-  assert(eng->tape[3] == 104);
-  assert(eng->tape[4] == 88);
-  assert(eng->tape[5] == 32);
-  assert(eng->tape[6] == 8);
+  exec("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]", 49);
+  assert(tape[0] == 0);
+  assert(tape[1] == 0);
+  assert(tape[2] == 72);
+  assert(tape[3] == 104);
+  assert(tape[4] == 88);
+  assert(tape[5] == 32);
+  assert(tape[6] == 8);
 
-  reset(eng);
+  reset();
   DBG_PRINT("tests::test 6 done");
 
   // // hello world
-  exec(eng,
-       "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>"
+  exec("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>"
        ">.<-.<.+++.------.--------.>>+.>++.",
        106);
 
-  reset(eng);
+  reset();
   DBG_PRINT("tests::test 7 done");
 
   return 0;
@@ -354,7 +387,7 @@ ProgStr read_file(char *file_path) {
   return prog_str;
 }
 
-int benchmark(engine *eng) {
+int benchmark(void) {
   clock_t start, end;
   double cpu_time_used;
 
@@ -363,13 +396,13 @@ int benchmark(engine *eng) {
 
   start = clock();
 
-  exec(eng, prog_str.prog, prog_str.len);
+  exec(prog_str.prog, prog_str.len);
 
   end = clock();
   cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
   DBG_PRINTF("benchmark::program 1 took: %f secs", cpu_time_used);
 
-  reset(eng);
+  reset();
 
   return 0;
 }
@@ -379,19 +412,18 @@ int main(int argc, char *argv[]) {
     printf("brenphuk [command]: repl, tests");
   }
 
-  engine eng;
-  memset(eng.tape, 0, sizeof eng.tape); // Make sure all tape is set to zero
-  eng.pointer = 0;
+  memset(tape, 0, sizeof tape); // Make sure all tape is set to zero
+  pointer = 0;
 
-  memset(eng.brackets_loc, -1, sizeof eng.brackets_loc);
+  memset(brackets_loc, -1, sizeof brackets_loc);
 
   char *cmd = argv[1];
   if (cmd == NULL || !strcmp(cmd, "repl")) {
-    return repl(&eng);
+    return repl();
   } else if (!strcmp(cmd, "tests")) {
-    tests(&eng);
+    tests();
   } else if (!strcmp(cmd, "benchmark")) {
-    benchmark(&eng);
+    benchmark();
   }
 
   return 0;
