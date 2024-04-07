88e535026ac08b11a7765e33076a2f49d4f707f3
feniljain <fkjainco@gmail.com>
2024-04-07 17:40:04 +0530

    feat: add simple dynasm_install script

---
+++ dynasm_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# exit if minilua already exists
+if [ ! -f minilua ]; then
+  echo "minilua file does not exist, cloning and building"
+else
+	exit 0
+fi
+
+git clone https://github.com/corsix/dynasm-doc.git
+cd dynasm-doc
+git submodule update --init
+gcc -o minilua luajit-2.0/src/host/minilua.c -lm
+
+cp minilua ../
